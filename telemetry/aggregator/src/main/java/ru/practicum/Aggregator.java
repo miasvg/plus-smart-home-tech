779bb685cc7f395d4ef0e4194d725a5c0e4027e1
Artiom Stadnijchuk <artiomstadnijchuk@gmail.com>
2025-08-25 20:19:58 +0500

    aggregator main fix

--- telemetry/aggregator/src/main/java/ru/practicum/Aggregator.java
+++ telemetry/aggregator/src/main/java/ru/practicum/Aggregator.java
@@ -2,10 +2,16 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.practicum.aggregation.AggregationStarter;
 
 @SpringBootApplication
 public class Aggregator {
     public static void main(String[] args) {
-        SpringApplication.run(Aggregator.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(Aggregator.class, args);
+
+        // Получаем бин AggregationStarter из контекста и запускаем основную логику сервиса
+        AggregationStarter aggregator = context.getBean(AggregationStarter.class);
+        aggregator.start();
     }
 }
